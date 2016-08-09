b7ee45ab3002669b7fb016de659c36f48ea216e6
flaumel <flaumel@gmail.com>
2016-08-09 18:44:18 +0200

    Updated F-Secure.py file for some kind of user interface.

--- F-Secure.py
+++ F-Secure.py
@@ -7,7 +7,22 @@ import os
 import datetime
 import time
 
-# Source: https://git.opnfv.org/cgit/yardstick/tree/yardstick/dispatcher/influxdb.py
+def showMenu():
+   print("1. Get information about all event times (in hours)")
+   print("2. Get amount of app launches")
+   print("3. Exit")
+   option = input("What would you like to do?\n")
+   logging.debug("Option chosen in menu is %s" % option)
+   return option
+
+'''
+ Function flattening multi-level dictionary
+ Arguments:
+           data - dictionary to flatten
+ Returns:
+           Flattened dictionary
+ Source: https://git.opnfv.org/cgit/yardstick/tree/yardstick/dispatcher/influxdb.py
+'''
 def _dict_key_flatten(data):
     next_data = {}
 
@@ -27,51 +42,70 @@ def _dict_key_flatten(data):
 
     return _dict_key_flatten(next_data)
 
-var = input("Would you like to make new database for events (yes/no)?\n")
-#if (var == 'yes'):
-#    os.remove('fSecure.db')
-#    logging.debug("Removed fSecure.db")
-#conn = sqlite3.connect('fSecure.db')
-conn = sqlite3.connect(':memory:')
+# Ask if user wants to create new database for events
+var = input("Would you like to delete old database for events and create new one? (yes/no)\n")
+if (var == 'yes'):
+    os.remove('fSecure.db')
+    logging.debug("Removed fSecure.db")
+
+# Open connection to database
+conn = sqlite3.connect('fSecure.db')
 logging.debug("Database created and opened successfully")
 c = conn.cursor()
-database.initializeDatabase(conn, c)
 
-lineNum = 1
-#var = input("Would you like to add more data 
+# Ask if user wants to add events data to database
+var = input("Would you like to add new events data into database (yes/no)?\n")
+if (var == 'yes'):
+    database.initializeDatabase(conn, c)
 
-with open('obfuscated_data', encoding="utf-8") as openfileobject:
-    for line in openfileobject:
-        decoded = json.loads(line)
-        decoded = _dict_key_flatten(decoded)
-        decoded_str = str(decoded)
-        database.insert_event_data(conn, c, decoded)
-#        print("Line %s" % lineNum)
-        lineNum = lineNum + 1
-        logging.basicConfig(filename='example.log', filemode='w',level=logging.DEBUG)
-        #logging.debug('This message should go to the log file')
-        #logging.info('So should this')
-        #logging.warning('And this, too')
-        if ( lineNum % 1000 == 0):
-            logging.warning('Processing event %s', lineNum)
-print("Done adding events to database!\n")
-c.execute('SELECT timestamp FROM eventDATA')
-timeAll = c.fetchall()
-hours = {}
-for time in timeAll:
-   hour = datetime.datetime.fromtimestamp(time[0]/1000).strftime("%H")
-   if hour in hours:
-       hours[hour] += 1
-   else:
-       hours[hour] = 1
-print(hours)
-
-c.execute('SELECT COUNT(*) FROM appData')
-appAmount = c.fetchone()[0]
-appNumber = input("There is %s applications in database.\nWhich one launches count you want to get?\n" % appAmount)
-str = 'launch'
-c.execute('SELECT name FROM appData WHERE appId=?', [appNumber])
-name = c.fetchone()[0]
-c.execute('SELECT appId FROM eventData WHERE (eventType=? AND appID=?)', [str, appNumber])
-apps = c.fetchall()
-print("Application %s has been launched %s times" % (name, len(apps)))
+    lineNum = 1
+    # Ask for filename with JSON events data
+    var = input("From what file would you like to read JSON events data?\n")
+    # Read file line by line, decode, flatten and insert into database
+    with open(var, encoding="utf-8") as openfileobject:
+        for line in openfileobject:
+            decoded = json.loads(line)
+            decoded = _dict_key_flatten(decoded)
+            decoded_str = str(decoded)
+            database.insert_event_data(conn, c, decoded)
+            lineNum = lineNum + 1
+            logging.basicConfig(filename='example.log', filemode='w',level=logging.DEBUG)
+            if (lineNum % 10000 == 0):
+                conn.commit()
+                logging.warning('Processing event %s', lineNum)
+    logging.warning("Done adding events to database!\n")
+    conn.commit()
+    
+opt = showMenu()
+while (opt != '3'):
+    if (opt == '1'):
+        logging.debug("Option 1")
+        c.execute('SELECT timestamp FROM eventDATA')
+        logging.warning("Getting data from eventData")
+        timeAll = c.fetchall()
+        hours = {}
+        logging.warning("Interpreting data")
+        for time in timeAll:
+            hour = datetime.datetime.fromtimestamp(time[0]/1000).strftime("%H")
+            if hour in hours:
+                hours[hour] += 1
+            else:
+                hours[hour] = 1
+        # Todo: print that dictionary in more user friendly way
+        print(hours)
+        opt = showMenu()
+    elif (opt == '2'):
+        logging.warning("Option 2")
+        c.execute('SELECT COUNT(*) FROM appData')
+        appAmount = c.fetchone()[0]
+        appNumber = input("There is %s applications in database.\nWhich one launches count you want to get?\n" % appAmount)
+        str = 'launch'
+        c.execute('SELECT name FROM appData WHERE appId=?', [appNumber])
+        name = c.fetchone()[0]
+        c.execute('SELECT appId FROM eventData WHERE (eventType=? AND appID=?)', [str, appNumber])
+        apps = c.fetchall()
+        print("Application %s has been launched %s times" % (name, len(apps)))
+        opt = showMenu()
+    else:
+        logging.warning("Else")
+        opt = showMenu()
